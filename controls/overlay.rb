7ef8187baa6d45ba420324b2cf9315fba5e96022
HackerShark <melsharkawi@mitre.org>
2020-03-23 14:30:34 -0400

    removing example, adding overlay
    Signed-off-by: HackerShark <melsharkawi@mitre.org>

---
+++ controls/overlay.rb
@@ -0,0 +1,225 @@
+include_controls 'oracle-database-12c-stig-baseline' do
+    control 'V-61579' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61671' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61867' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+          skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61441' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+         skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61447' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61489' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61511' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61515' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61531' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61533' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61535' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61601' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61965' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61685' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61703' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61705' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61707' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61709' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61713' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61715' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61733' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61737' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61543' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61741' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61743' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61747' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61749' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61783' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61815' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61545' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61759' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+
+    control 'V-61787' do
+      impact 0.0
+      describe 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on' do
+        skip 'This control is not applicable on oracle within aws rds, as aws manages the operating system in which the oracle database is running on'
+      end
+    end
+  end
